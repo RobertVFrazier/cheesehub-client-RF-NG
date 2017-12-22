401d9968768daa7e68846696ee801b98df3b9fd4
Robert Frazier <robert.v.frazier@gmail.com>
2017-12-22 16:52:10 -0500

    Program up and running.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    API_BASE_URL: 'http://localhost:8080',
+    API_BASE_URL:
+        process.env.REACT_APP_API_BASE_URL="https://cheese-list-rf-ng.herokuapp.com" ||  
+        'http://localhost:8080',
     PORT: process.env.PORT || 8080,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     DATABASE_URL:
